7312f759feec41c7f304256a1f6034543109ce01
hrh4real <himanshuhada167@gmail.com>
2025-02-25 21:39:01 +0530

    Add Dice component and App set up

---
+++ App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState, useRef, useEffect } from "react";
+import Dice from "./src/components/Dice";
+
+export default function App() {
+    const [numbers, setNumbers] = useState(RANDOM_NUMBER())
+    function RANDOM_NUMBER () {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false
+            }))
+        // OR 
+        // return Math.floor(Math.random() * 6) + 1
+    }
+    function rollDice() {
+        setNumbers(RANDOM_NUMBER())
+    }
+    return (
+            <main>
+                <div className="dice-container">
+                {numbers.map((num, index) => <Dice key={num.index} value={num.value} />)}
+                </div>
+                <button
+                    onClick={rollDice}
+                    className="roll-dice"
+                >
+                    Roll Dice
+                </button>
+            </main>
+    )
+}
